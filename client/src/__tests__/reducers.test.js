e4afbc870077f711315e7198e2a0c0acf3677792
Deku <zachdsamson@gmail.com>
2021-11-08 11:17:29 -0700

    initialize global state reducer

--- client/src/__tests__/reducers.test.js
+++ client/src/__tests__/reducers.test.js
@@ -22,3 +22,23 @@ test('UPDATE_PRODUCTS', () => {
     expect(newState.products.length).toBe(2);
     expect(initialState.products.length).toBe(0);
 });
+
+test('UPDATE_CATEGORIES', () => {
+    let newState = reducer(initialState, {
+        type: UPDATE_CATEGORIES,
+        categories: [{}, {}]
+    });
+
+    expect(newState.categories.length).toBe(2);
+    expect(initialState.categories.length).toBe(1);
+});
+
+test('UPDATE_CURRENT_CATEGORY', () => {
+    let newState = reducer(initialState, {
+        type: UPDATE_CURRENT_CATEGORY,
+        currentCategory: '2'
+    });
+
+    expect(newState.currentCategory).toBe('2');
+    expect(initialState.currentCategory).toBe('1');
+});
